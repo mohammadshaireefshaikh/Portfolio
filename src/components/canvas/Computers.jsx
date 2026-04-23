b0e3de297328da592ed4d78517497a7f0a706693
Mohammad Shaikh <shoabshaikh10@gmail.com>
2026-04-23 17:28:06 +0100

    Update Computers.jsxfix: use absolute path for desktop_pc GLTF model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = () => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+    const computer = useGLTF("/Portfolio/desktop_pc/scene.gltf");
 
   return (
     <mesh>
